2443b4d2283fa1d4fb3072cf8108a6c23b82e606
jamiekim828 <jamiekim828@gmail.com>
2019-06-11 14:11:47 +0200

    User state toggle button add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './Title'
+import LightSwitch from './LightSwitch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  //define the method render
+  render() {
+
+    //what it should return
+    //React calls this App.render()
+    //JSX <h1>Hallo!</h1>
+    //you can not return undefined
+    //you can return null if you want
+    //strings, numbers, array of strings or react components
+    
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Greeting />
+         {/* you can insert values using curly braces*/}
+         {/* what values are alloweded? */}
+          { 'Welcome to React' }
+          {/* { 5 + 5 } */}
+          {/* {null}
+          { true }
+          { [] }
+          { [<Greeting />, <Greeting />, <Greeting />]} */}
+          
+        </header>
+
+        <main>
+          <Title content="This is the Title"/>
+          
+          <div>
+            <h3>User Board</h3>
+          </div>
+          <div>
+            <p>
+            "User: "<b>Alice</b>
+            </p>
+            <LightSwitch />
+          </div>
+          <div>
+            <p>
+            "User: "<b>Bob</b>
+            </p>
+            <LightSwitch />
+          </div>
+          <div>
+            <p>
+            "User: "<b>Chloe</b>
+            </p>
+            <LightSwitch />
+          </div>
+          
+        </main>
+      </div>
+  )
+  }
+}
+ 
+class Greeting extends React.Component {
+  render() {
+    return(
+      <h1 className="App-title">Hallo!</h1>
+    )
+  }
 }
 
 export default App;
---
+++ src/LightSwitch.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class LightSwitch extends Component {
+    state = { active: true }
+  
+    toggle = () => {
+        this.setState({
+          active: !this.state.active
+        })
+    }
+
+    render() {
+      return (
+      <div>
+        <p>The User is 
+            <b>{ this.state.active ? ' ' : ' not' }
+            </b> present
+        </p>
+        <button onClick={this.toggle}>Toggle</button>
+      </div>
+      )
+    }
+  }
